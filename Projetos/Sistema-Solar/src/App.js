7de9f74be5d6b93a17ef3451ca2681c1a1a03fc2
Leandro Teixeira <leandroteixeira3@gmail.com>
2022-01-26 18:05:44 -0300

    Projeto Sistema Solar adicionado

---
+++ Projetos/Sistema-Solar/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
+import Header from './components/header';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Header />
+        <SolarSystem />
+        <Missions />
+      </div>);
+  }
+}
+
+export default App;
